17be3fb81878f967574364915deba652aeb0a80e
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-04-19 20:09:31 -0300

    Estrutura da aplicação - Componente: Header
    Nessa aula, vamos finalizar a estrutura e estilização do Header da nossa aplicação utilizando as configurações de cores e CSS que fizemos anteriormente.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css';
 
+import logoIgnite from '../assets/logo-ignite.svg';
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={logoIgnite} alt="Logo do Ignite" />
+      <h1>Ignite Feed</h1>
     </header>
   );
 } 
