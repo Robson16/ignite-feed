aa38da73e125033d4e49efa5d03f7801d7025dce
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-04-20 16:48:12 -0300

    Estrutura da aplicação - Componente: Post
    Nessa aula vamos estruturar o conteúdo do componente Post, nos baseando no conteúdo que podemos encontrar no Layout do Figma.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from './App.module.css';
 import { Header } from './components/Header';
+import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 import './global.css';
 
@@ -11,8 +12,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quos, consectetur in unde eius aliquam, iure nesciunt dicta voluptas culpa illum quas! Ex aspernatur veritatis quasi nobis. Ipsa, consequuntur ea!</p>
-          <p>Neque fugit ullam officiis tempora. Perspiciatis consequuntur ratione, aliquid harum recusandae quam excepturi, ea nesciunt a necessitatibus atque voluptas magni voluptatum dolorem quod eum nam aut quaerat architecto explicabo! Nostrum.</p>
+          <Post />
+          <Post />
         </main>
       </div>
     </>
---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/Robson16.png"
+            alt="Avatar"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Robson H. Rodrigues</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time
+          title="24 de Outubro às 08:13"
+          dateTime="2023-10-24 08:13:00"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galera, tudo certo?</p>
+        <p>
+          Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz
+          no evento NextLevelWeek #14 da RockeatSeat, onde desenvolvi uma aplicação de anotações.
+        </p>
+        <p>
+          <a href="https://github.com/Robson16/nlw14-notes">👉 github.com/Robson16/nlw14-notes</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a>
+          <a href="#">#ignite</a>
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
