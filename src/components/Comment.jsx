4df598690dab7616f586d90a44f53895d723f38b
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-04-21 16:39:32 -0300

    Estrutura da aplicação - Componente: Comment
    Agora vamos começar a criar a estrutura do nosso componente de comentário, que exibirá cada comentário de um post do nosso feed.

---
+++ src/components/Comment.jsx
@@ -0,0 +1,41 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src="https://github.com/Robson16.png"
+        alt="Avatar"
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Robson H. Rodrigues</strong>
+              <time
+                title="24 de Outubro às 08:13"
+                dateTime="2023-10-24 08:13:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div >
+    </div >
+  );
+}
